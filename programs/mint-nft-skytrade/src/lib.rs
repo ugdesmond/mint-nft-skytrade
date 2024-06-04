7a18fe844e4c2ed50a8189705839e28cfea51b14
Ugochukwu Okoro <ugdesmond@gmail.com>
2024-06-04 22:18:02 +0100

    mint nft to collection

--- programs/mint-nft-skytrade/src/lib.rs
+++ programs/mint-nft-skytrade/src/lib.rs
@@ -1,15 +1,137 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    metadata::{Metadata, MetadataAccount},
+    token::Mint,
+};
+use mpl_bubblegum::{
+    cpi::{
+        accounts::{  MintToCollectionV1},
+         mint_to_collection_v1,
+    },
+    program::Bubblegum,
+    state::metaplex_adapter::{
+        Collection, Creator, MetadataArgs, TokenProgramVersion, TokenStandard,
+    },
+};
+use solana_program::pubkey::Pubkey;
+use spl_account_compression::{program::SplAccountCompression, Noop};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+declare_id!("AECLhMQ7QB11Ugxze54bQ535LL6V53RceEfPbBedzrSf");
+
+pub const SEED: &str = "AUTH";
+
 
 #[program]
 pub mod mint_nft_skytrade {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint_nft(context: Context<MintNftToCollection>) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[SEED.as_bytes(), &[*context.bumps.get("pda").unwrap()]]];
+
+
+        // use collection nft metadata as the metadata for the compressed nft
+        let metadata_account = &context.accounts.collection_metadata;
+        let metadata = MetadataArgs {
+            // The name of the asset
+            name: metadata_account.data.name.to_string(),
+            // The symbol for the asset
+            symbol: metadata_account.data.symbol.to_string(),
+            // URI pointing to JSON representing the asset
+            uri: metadata_account.data.uri.to_string(),
+            // Collection
+            collection: Some(Collection {
+                key: context.accounts.collection_mint.key(),
+                verified: false,
+            }),
+            primary_sale_happened: true,
+            is_mutable: true,
+            edition_nonce: None,
+            token_standard: Some(TokenStandard::NonFungible),
+            uses: None,
+            token_program_version: TokenProgramVersion::Original,
+            // set creator as pda
+            creators: vec![Creator {
+                address: context.accounts.pda.key(), 
+                verified: true,
+                share: 100,
+            }],
+            seller_fee_basis_points: 0,
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            context.accounts.bubblegum_program.to_account_info(),
+            MintToCollectionV1 {
+                tree_authority: context.accounts.tree_authority.to_account_info(),
+                leaf_owner: context.accounts.payer.to_account_info(),
+                leaf_delegate: context.accounts.payer.to_account_info(),
+                merkle_tree: context.accounts.merkle_tree.to_account_info(),
+                payer: context.accounts.payer.to_account_info(),
+                tree_delegate: context.accounts.pda.to_account_info(), 
+                collection_authority: context.accounts.pda.to_account_info(), 
+                collection_authority_record_pda: context.accounts.bubblegum_program.to_account_info(),
+                collection_mint: context.accounts.collection_mint.to_account_info(), 
+                collection_metadata: context.accounts.collection_metadata.to_account_info(),
+                edition_account: context.accounts.edition_account.to_account_info(), 
+                bubblegum_signer: context.accounts.bubblegum_signer.to_account_info(),
+                log_wrapper: context.accounts.log_wrapper.to_account_info(),
+                compression_program: context.accounts.compression_program.to_account_info(),
+                token_metadata_program: context.accounts.token_metadata_program.to_account_info(),
+                system_program: context.accounts.system_program.to_account_info(),
+            },
+            signer_seeds,
+        );
+
+        mint_to_collection_v1(cpi_ctx, metadata)?;
+
         Ok(())
     }
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintNftToCollection<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// CHECK:
+    #[account(
+        seeds = [SEED.as_bytes()],
+        bump,
+    )]
+    pub pda: UncheckedAccount<'info>,
+
+    /// CHECK:
+    #[account(
+        mut,
+        seeds = [merkle_tree.key().as_ref()],
+        bump,
+        seeds::program = bubblegum_program.key()
+    )]
+    pub tree_authority: UncheckedAccount<'info>,
+
+    /// CHECK:
+    #[account(mut)]
+    pub merkle_tree: UncheckedAccount<'info>,
+
+    /// CHECK:
+    #[account(
+        seeds = ["collection_cpi".as_bytes()],
+        seeds::program = bubblegum_program.key(),
+        bump,
+    )]
+    pub bubblegum_signer: UncheckedAccount<'info>,
+
+    pub log_wrapper: Program<'info, Noop>,
+    pub compression_program: Program<'info, SplAccountCompression>,
+    pub bubblegum_program: Program<'info, Bubblegum>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub system_program: Program<'info, System>,
+
+    pub collection_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub collection_metadata: Account<'info, MetadataAccount>,
+    /// CHECK:
+    pub edition_account: UncheckedAccount<'info>,
+}
--- tests/mint-nft-skytrade.ts
+++ tests/mint-nft-skytrade.ts
@@ -1,16 +1,149 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { MintNftSkytrade } from "../target/types/mint_nft_skytrade";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import {
+  CreateNftOutput,
+  Metaplex,
+  keypairIdentity,
+} from '@metaplex-foundation/js';
+import { PROGRAM_ID as BUBBLEGUM_PROGRAM_ID } from '@metaplex-foundation/mpl-bubblegum';
+import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
 
-describe("mint-nft-skytrade", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+import {
+  SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+  SPL_NOOP_PROGRAM_ID,
+  ValidDepthSizePair,
+  createAllocTreeIx,
+} from '@solana/spl-account-compression';
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import { MintNftSkytrade } from '../target/types/mint_nft_skytrade';
+import { extractAssetId } from '../utils/utils';
 
+describe('mint-nft-skytrade', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.MintNftSkytrade as Program<MintNftSkytrade>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // const connection = program.provider.connection
+  const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+  const metaplex = Metaplex.make(connection).use(keypairIdentity(wallet.payer));
+
+  // keypair for tree
+  const merkleTree = Keypair.generate();
+
+  // tree authority
+  const [treeAuthority] = PublicKey.findProgramAddressSync(
+    [merkleTree.publicKey.toBuffer()],
+    BUBBLEGUM_PROGRAM_ID
+  );
+
+  // pda "tree creator", allows our program to update the tree
+  const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from('AUTH')],
+    program.programId
+  );
+
+  const [bubblegumSigner] = PublicKey.findProgramAddressSync(
+    [Buffer.from('collection_cpi', 'utf8')],
+    BUBBLEGUM_PROGRAM_ID
+  );
+
+  const maxDepthSizePair: ValidDepthSizePair = {
+    maxDepth: 14,
+    maxBufferSize: 64,
+  };
+  const canopyDepth = maxDepthSizePair.maxDepth - 5;
+
+  const metadata = {
+    uri: 'https://arweave.net/h19GMcMz7RLDY7kAHGWeWolHTmO83mLLMNPzEkF32BQ',
+    name: 'SKY-TRADE',
+    symbol: 'SKY',
+  };
+
+  let collectionNft: CreateNftOutput;
+  let assetId: PublicKey;
+
+  before(async () => {
+    // Create collection nft
+    collectionNft = await metaplex.nfts().create({
+      uri: metadata.uri,
+      name: metadata.name,
+      symbol: metadata.symbol,
+      sellerFeeBasisPoints: 0,
+      isCollection: true,
+    });
+
+    // transfer collection nft metadata update authority to pda
+    await metaplex.nfts().update({
+      nftOrSft: collectionNft.nft,
+      updateAuthority: wallet.payer,
+      newUpdateAuthority: pda,
+    });
+
+    // instruction to create new account with required space for tree
+    const allocTreeIx = await createAllocTreeIx(
+      connection,
+      merkleTree.publicKey,
+      wallet.publicKey,
+      maxDepthSizePair,
+      canopyDepth
+    );
+
+    const tx = new Transaction().add(allocTreeIx);
+
+    const txSignature = await sendAndConfirmTransaction(
+      connection,
+      tx,
+      [wallet.payer, merkleTree],
+      {
+        commitment: 'confirmed',
+      }
+    );
+    console.log(
+      `==============https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+    );
+  });
+
+  it('Mint  NFT with Metaplex Bubblegum standard', async () => {
+    try {
+      console.log('====minting nft====', collectionNft.metadataAddress);
+      const txSignature = await program.methods
+        .mintNft()
+        .accounts({
+          pda: pda,
+          merkleTree: merkleTree.publicKey,
+          treeAuthority: treeAuthority,
+          logWrapper: SPL_NOOP_PROGRAM_ID,
+          bubblegumSigner: bubblegumSigner,
+          bubblegumProgram: BUBBLEGUM_PROGRAM_ID,
+          compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+          tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+
+          collectionMint: collectionNft.mintAddress,
+          collectionMetadata: collectionNft.metadataAddress,
+          editionAccount: collectionNft.masterEditionAddress,
+        })
+        .rpc({ commitment: 'confirmed' });
+      console.log(
+        `>>>>>>>>>>>>>>>>.https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+      );
+
+      assetId = await extractAssetId(
+        connection,
+        txSignature,
+        merkleTree.publicKey,
+        program.programId
+      );
+    } catch (error) {
+      console.log('======error thrown=====', error);
+    }
   });
 });
